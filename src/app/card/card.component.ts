735e3f3db12b96df16980347aeb939cb600f982f
Rahul Tandel <rahulkv395@gmail.com>
2022-07-23 10:57:45 +0530

    Changed Whole UI

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.getRandomPhoto();
   }
 
+  getRandomPhoto() {}
 }
